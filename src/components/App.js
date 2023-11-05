806598ef6dd4fd065cf136c87c0b3747ba5b3566
storm <stormarasa2@gmail.com>
2023-11-05 22:57:45 +0300

    Initial Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((res) => res.json())
+    .then((questions) => setQuestions(questions))
+  }, [])
+
+  function handleAddQuestion(formData) {
+    fetch('http://localhost:4000/questions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        prompt: formData.prompt,
+        answers: [
+          formData.answer1,
+          formData.answer2,
+          formData.answer3,
+          formData.answer4,
+        ],
+        correctIndex: parseInt(formData.correctIndex)
+      })
+    })
+    .then(response => response.json())
+    .then((question) => setQuestions([...questions, question]))
+  }
 
   return (
     <main>
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,26 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+
+function QuestionList({questions, handleDelete, handleUpdate }) {
+  // const [questions, setQuestions] = useState([]);
+  // // Fetch questions when component mounts
+  // useEffect(() => {
+  //   fetch("http://localhost:4000/questions")
+  //   .then((res) => res.json())
+  //   .then((questions) => setQuestions(questions))
+  // })
+  const questionList = questions.map((question) => {
+    return <QuestionItem key={question.id} question={question} onHandleDelete={() => onHandleDelete(question.id)} onHandleUpdate={(newCorrectIndex) => onHandleUpdate(question.id, newCorrectIndex)}/> 
+  })
+  
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{}</ul>
     </section>
   );
 }
 
 export default QuestionList;
+
